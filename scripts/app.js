582664e77b2c26241148c68c416ad7232829f2e1
obay2 <obaymardini@gmail.com>
2016-10-24 02:50:38 +0200

    radion buttons are working fine

--- scripts/app.js
+++ scripts/app.js
@@ -150,24 +150,26 @@ angular.module('confusionApp', [])
             
             $scope.dish = dish;
             
+            
+            
         }])
 
-//        .controller('DishCommentController', ['$scope', function($scope) {
-//            
-//            //Step 1: Create a JavaScript object to hold the comment from the form
-//            
-//            $scope.submitComment = function () {
-//                
-//                //Step 2: This is how you record the date
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.commentFormObj = {rating: 5}
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
 //                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
-//                
-//                // Step 3: Push your comment into the dish's comment array
-//                $scope.dish.comments.push("Your JavaScript Object holding the comment");
-//                
-//                //Step 4: reset your form to pristine
-//                
-//                //Step 5: reset your JavaScript object that holds your comment
-//            }
-//        }])
+                
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                
+                //Step 4: reset your form to pristine
+                
+                //Step 5: reset your JavaScript object that holds your comment
+            }
+        }])
 
 ;
